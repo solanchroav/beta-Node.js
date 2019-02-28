c8d32feb240677f3ef19665d62497d298dd69b81
solanchroav <32616169+solanchroav@users.noreply.github.com>
2019-02-28 10:56:28 -0300

    carga inicial

---
+++ js/pruebaBackEnd.js
@@ -0,0 +1,18 @@
+var arrUsuario = [
+  {
+    id:1,
+    nombre: "Pablo"
+  },
+  {
+    id:2,
+    nombre: "Agustina"
+  },
+  {
+    id:3,
+    nombre: "Solanch"
+  }
+];
+
+for (var i = 0; i < arrUsuario.length; i++) {
+  console.log(arrUsuario[i].nombre);
+}
